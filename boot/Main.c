8ca286682d7d1883c0f9fedd1c7c0d6fb0a741aa
Joons <elddy0948@gmail.com>
2023-12-20 23:27:38 +0900

    Uart print test

--- boot/Main.c
+++ boot/Main.c
@@ -1,8 +1,18 @@
-#include "stdint.h"
+#include "HalUart.h"
+
+static void Hw_init(void);
 
 void main(void)
 {
-    uint32_t* dummyAddr = (uint32_t*)(1024 * 1024 * 100);
+    Hw_init();
+    uint32_t i = 100;
+    while (i--)
+    {
+        Hal_uart_put_char('N');
+    }
+}
 
-    *dummyAddr = sizeof(long); 
+static void Hw_init(void)
+{
+    Hal_uart_init();
 }
