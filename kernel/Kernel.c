348db911cb7916500c60d49b1a456fa8bb7f434a
joons <elddy0948@gmail.com>
2024-02-13 19:04:43 +0900

    feat: send, wait events

--- kernel/Kernel.c
+++ kernel/Kernel.c
@@ -1,6 +1,8 @@
-#include "../include/stdint.h"
-#include "Kernel.h"
+#include "../include/memio.h"
 #include "task.h"
+
+#include "Kernel.h"
+
 
 void Kernel_start(void)
 {
@@ -11,4 +13,34 @@ void Kernel_yield(void)
 {
 	// when task done, just call scheduler 
 	Kernel_task_scheduler();
+}
+
+void Kernel_send_events(uint32_t event_list)
+{
+	uint32_t i = 0;
+
+	for (i = 0; i < 32; ++i)
+	{
+		if ((event_list >> i) & 1)
+		{
+			KernelEventFlag_t sending_event = KernelEventFlag_Empty;
+			sending_event = (KernelEventFlag_t)SET_BIT(sending_event, i);
+			Kernel_event_flag_set(sending_event);
+		}
+	}
+}
+
+KernelEventFlag_t Kernel_wait_events(uint32_t waiting_list)
+{
+	uint32_t i = 0;
+	
+	for (i = 0; i < 32; ++i)
+	{
+		KernelEventFlag_t waiting_event = KernelEventFlag_Empty;
+		waiting_event = (KernelEventFlag_t)SET_BIT(waiting_event, i);
+		if (Kernel_event_flag_check(waiting_event))
+			return waiting_event;
+	}
+
+	return KernelEventFlag_Empty;
 }
--- kernel/Kernel.h
+++ kernel/Kernel.h
@@ -1,7 +1,18 @@
 #ifndef KERNEL_KERNEL_H_
 #define KERNEL_KERNEL_H_
 
+#include "../include/stdint.h"
+#include "event.h"
+
 void Kernel_start(void);
 void Kernel_yield(void);
+
+// Event API
+
+// for sending event 
+void Kernel_send_events(uint32_t event_list);
+
+// waiting for event (don't care who sent)
+KernelEventFlag_t Kernel_wait_events(uint32_t waiting_list);
 
 #endif /* KERNEL_KERNEL_H_ */
