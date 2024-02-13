27d6f6eeaedb1b82b990b3f70a73e3fcc38da969
joons <elddy0948@gmail.com>
2024-02-13 19:04:18 +0900

    feat: kernel event flag set, clear, check

---
+++ kernel/event.c
@@ -0,0 +1,42 @@
+// Include
+#include "../include/stdint.h"
+#include "../include/stdbool.h"
+
+// Lib
+#include "../lib/stdio.h"
+#include <wctype.h>
+
+// Kernel
+#include "./event.h"
+
+static uint32_t sEventFlag;
+
+void Kernel_event_flag_init(void)
+{
+	// event flag initialize
+	sEventFlag = 0x00000000;
+}
+
+void Kernel_event_flag_set(KernelEventFlag_t event)
+{
+	// set occured event bit
+	sEventFlag |= (uint32_t)event;
+}
+
+void Kernel_event_flag_clear(KernelEventFlag_t event)
+{
+	// clear event bit
+	sEventFlag &= ~((uint32_t)event);
+}
+
+bool Kernel_event_flag_check(KernelEventFlag_t event)
+{
+	if (sEventFlag & (uint32_t)event)
+	{
+		// if event flag is more than 0 
+		Kernel_event_flag_clear(event);
+		return true;
+	}
+
+	return false;
+}
