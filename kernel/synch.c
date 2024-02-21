b1a0921df9a10950e4f786f1c194ad7e45053e9a
Hojoon Kim <elddy0948@gmail.com>
2024-02-21 23:22:10 +0900

    feat: implement feature for mutex in synch

--- kernel/synch.c
+++ kernel/synch.c
@@ -4,6 +4,7 @@
 
 static int32_t sSemMax;
 static int32_t sSem;
+static KernelMutex_t sMutex;
 
 void Kernel_sem_init(int32_t max)
 {
@@ -27,5 +28,31 @@ void Kernel_sem_release()
 		sSem = sSemMax;
 
 	sSem++;
+}
+
+void Kernel_mutex_init(void)
+{
+	sMutex.owner = 0;
+	sMutex.lock = false;
+}
+
+bool Kernel_mutex_lock(uint32_t owner)
+{
+	if (sMutex.lock)
+		return false;
+
+	sMutex.owner = owner;
+	sMutex.lock = true;
+	return true;
+}
+
+bool Kernel_mutex_unlock(uint32_t owner)
+{
+	if (owner == sMutex.owner)
+	{
+		sMutex.lock = false;
+		return true;
+	}
+	return false;
 }
 
